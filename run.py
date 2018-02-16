8828af339f42ea0a3c22345d2fbaa4e719cfde46
mes32 <mes32@users.noreply.github.com>
2018-02-15 20:28:42 -0500

    refactor: Add comments and docstring to run.py.

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+'''
+Random number generator application
+'''
 
 import random
 
-rolled_number = random.randint(1, 20)
+# 1. Set range of random integers
+min_randint = 1
+max_randint = 20
+
+# 2. Generate a random integer
+rolled_number = random.randint(min_randint, max_randint)
+
+# 3. Print the number
 print(rolled_number)
