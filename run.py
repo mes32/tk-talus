5cc5d70388d710cf7f4439ec74a39969522801fa
mes32 <mes32@users.noreply.github.com>
2018-02-15 20:51:57 -0500

    feature: Display result inside a GUI window.

--- run.py
+++ run.py
@@ -4,13 +4,23 @@ Random number generator application
 '''
 
 import random
+import tkinter
 
-# 1. Set range of random integers
+# Range of random integers
 min_randint = 1
 max_randint = 20
 
-# 2. Generate a random integer
-rolled_number = random.randint(min_randint, max_randint)
+# Generate a random integer
+randint_as_int = random.randint(min_randint, max_randint)
+randint_as_str = str(randint_as_int)
 
-# 3. Print the number
-print(rolled_number)
+# Create a new graphical user interface (GUI) using tkinter
+gui = tkinter.Tk()
+
+# Place the random integer as text inside the GUI window
+window = tkinter.Label(gui, text=randint_as_str)
+window.pack()
+
+# Show the GUI to the user. Start the GUI function 'mainloop()'.
+print(" - NOTE: The GUI window is very small. Check the top left of the screen.")
+gui.mainloop()
